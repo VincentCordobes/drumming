2507c25ed803116a78f7f37a9b91ca653065960a
Vincent Cordobes <vincent.cordobes@gmail.com>
2018-07-26 09:21:35 +0200

    choose one and write to done file

--- src/choose-one.js
+++ src/choose-one.js
@@ -1,6 +1,22 @@
-const fs = require('fs')
-const videos = fs.readdirSync('./')
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const { spawn } = require("child_process");
+const { DateTime } = require("luxon");
 
-const random = Math.floor(Math.random() * videos.length-1)
-const pickedVideo = videos[random]
-console.log(pickedVideo)
+const VIDEO_DIR = "./saved_videos";
+const TODO_DRUM_FILE = "Dropbox/wiki/drum_todo.md";
+
+const videos = fs.readdirSync(VIDEO_DIR);
+
+const random = Math.floor(Math.random() * videos.length - 1);
+const pickedVideo = videos[random];
+console.log("Picked video:", pickedVideo);
+
+console.log("Adding it to drum_todo...");
+const item = `
+${pickedVideo}
+> ${DateTime.local().toISODate()}
+`;
+const filePath = path.join(os.homedir(), TODO_DRUM_FILE);
+fs.appendFileSync(filePath, item);
